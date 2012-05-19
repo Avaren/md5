66acf9ae0539d4c2e86e32d96f4ccf52a34b755b
Toby Gilham <Tobias1087@gmail.com>
2012-05-19 23:52:14 +0100

    maybe did the MD5

--- md5.py
+++ md5.py
@@ -71,7 +71,7 @@ I = lambda X, Y, Z: Y ^ (X | ~Z)
 
 X = modifiable_bitstring
 #    4294967296 == 2**32
-T = [int(floor(4294967296 * abs(sin(i)))) for i in range(1, 65)]
+T = [int(floor(4294967296 * abs(sin(i)))) for i in range(0, 65)]
 
 AA = copy(A)
 BB = copy(B)
@@ -90,22 +90,91 @@ def op(a, b, c, d, k, s, i, f):
     return BitArray(uint=result, length=32)
 
 # round 1
-A = op(A,B,C,D, k=0, s=7, i=1, f=F)
-D = op(D,A,B,C, k=0, s=7, i=1, f=F)
-C = op(C,D,A,B, k=0, s=7, i=1, f=F)
-B = op(B,C,D,A, k=0, s=7, i=1, f=F)
+A = op(A,B,C,D, k=0, s=7 , i=1, f=F)
+D = op(D,A,B,C, k=1, s=12, i=2, f=F)
+C = op(C,D,A,B, k=2, s=17, i=3, f=F)
+B = op(B,C,D,A, k=3, s=22, i=4, f=F)
 
-A = op(A,B,C,D, k=0, s=7, i=1, f=F)
-D = op(D,A,B,C, k=0, s=7, i=1, f=F)
-C = op(C,D,A,B, k=0, s=7, i=1, f=F)
-B = op(B,C,D,A, k=0, s=7, i=1, f=F)
+A = op(A,B,C,D, k=4, s=7 , i=5, f=F)
+D = op(D,A,B,C, k=5, s=12, i=6, f=F)
+C = op(C,D,A,B, k=6, s=17, i=7, f=F)
+B = op(B,C,D,A, k=7, s=22, i=8, f=F)
 
-A = op(A,B,C,D, k=0, s=7, i=1, f=F)
+A = op(A,B,C,D, k=8 , s=7 , i=9, f=F)
+D = op(D,A,B,C, k=9 , s=12, i=10, f=F)
+C = op(C,D,A,B, k=10, s=17, i=11, f=F)
+B = op(B,C,D,A, k=11, s=22, i=12, f=F)
 
-D = op(D,A,B,C, k=0, s=7, i=1, f=F)
-C = op(C,D,A,B, k=0, s=7, i=1, f=F)
-B = op(B,C,D,A, k=0, s=7, i=1, f=F)
-A = op(A,B,C,D, k=0, s=7, i=1, f=F)
-D = op(D,A,B,C, k=0, s=7, i=1, f=F)
-C = op(C,D,A,B, k=0, s=7, i=1, f=F)
-B = op(B,C,D,A, k=0, s=7, i=1, f=F)
+A = op(A,B,C,D, k=12, s=7 , i=13, f=F)
+D = op(D,A,B,C, k=13, s=12, i=14, f=F)
+C = op(C,D,A,B, k=14, s=17, i=15, f=F)
+B = op(B,C,D,A, k=15, s=22, i=16, f=F)
+
+# round 2
+A = op(A,B,C,D, k=1 , s=5 , i=17, f=G)
+D = op(D,A,B,C, k=6 , s=9 , i=18, f=G)
+C = op(C,D,A,B, k=11, s=14, i=19, f=G)
+B = op(B,C,D,A, k=0 , s=20, i=20, f=G)
+
+A = op(A,B,C,D, k=5 , s=5 , i=21, f=G)
+D = op(D,A,B,C, k=10, s=9 , i=22, f=G)
+C = op(C,D,A,B, k=15, s=14, i=23, f=G)
+B = op(B,C,D,A, k=4 , s=20, i=24, f=G)
+
+A = op(A,B,C,D, k=9 , s=5 , i=25, f=G)
+D = op(D,A,B,C, k=14, s=9 , i=26, f=G)
+C = op(C,D,A,B, k=3 , s=14, i=27, f=G)
+B = op(B,C,D,A, k=8 , s=20, i=28, f=G)
+
+A = op(A,B,C,D, k=13, s=5 , i=29, f=G)
+D = op(D,A,B,C, k=2 , s=9 , i=30, f=G)
+C = op(C,D,A,B, k=7 , s=14, i=31, f=G)
+B = op(B,C,D,A, k=12, s=20, i=32, f=G)
+
+# round 3
+A = op(A,B,C,D, k=5 , s=4 , i=33, f=H)
+D = op(D,A,B,C, k=8 , s=11, i=34, f=H)
+C = op(C,D,A,B, k=11, s=16, i=35, f=H)
+B = op(B,C,D,A, k=14, s=23, i=36, f=H)
+
+A = op(A,B,C,D, k=1 , s=4 , i=37, f=H)
+D = op(D,A,B,C, k=4 , s=11, i=38, f=H)
+C = op(C,D,A,B, k=7 , s=16, i=39, f=H)
+B = op(B,C,D,A, k=10, s=23, i=40, f=H)
+
+A = op(A,B,C,D, k=13, s=4 , i=41, f=H)
+D = op(D,A,B,C, k=0 , s=11, i=42, f=H)
+C = op(C,D,A,B, k=3 , s=16, i=43, f=H)
+B = op(B,C,D,A, k=6 , s=23, i=44, f=H)
+
+A = op(A,B,C,D, k=9 , s=4 , i=45, f=H)
+D = op(D,A,B,C, k=12, s=11, i=46, f=H)
+C = op(C,D,A,B, k=15, s=16, i=47, f=H)
+B = op(B,C,D,A, k=2 , s=23, i=48, f=H)
+
+# round 4
+A = op(A,B,C,D, k=0 , s=5 , i=49, f=I)
+D = op(D,A,B,C, k=7 , s=9 , i=50, f=I)
+C = op(C,D,A,B, k=14, s=14, i=51, f=I)
+B = op(B,C,D,A, k=5 , s=20, i=52, f=I)
+
+A = op(A,B,C,D, k=12, s=5 , i=53, f=I)
+D = op(D,A,B,C, k=3 , s=9 , i=54, f=I)
+C = op(C,D,A,B, k=10, s=14, i=55, f=I)
+B = op(B,C,D,A, k=1 , s=20, i=56, f=I)
+
+A = op(A,B,C,D, k=8 , s=5 , i=57, f=I)
+D = op(D,A,B,C, k=15, s=9 , i=58, f=I)
+C = op(C,D,A,B, k=6 , s=14, i=59, f=I)
+B = op(B,C,D,A, k=13, s=20, i=60, f=I)
+
+A = op(A,B,C,D, k=4 , s=5 , i=61, f=I)
+D = op(D,A,B,C, k=11, s=9 , i=62, f=I)
+C = op(C,D,A,B, k=2 , s=14, i=63, f=I)
+B = op(B,C,D,A, k=9 , s=20, i=64, f=I)
+
+A = A + AA
+B = B + BB
+C = C + CC
+D = D + DD
+print "{} {} {} {}".format(A,B,C,D)
